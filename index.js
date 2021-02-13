6e4633ea7f1ea70e8c97dfff1c6b4013d6ec0d8a
Aaron Stroud <astroud@gmail.com>
2021-02-13 11:53:33 -0800

    feat: add React frontend

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 
 const app = express()
 
 
-app.use(cors())
+// app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {

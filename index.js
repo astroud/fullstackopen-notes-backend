6f2b40f8ce4578433ad80d71ef0fe060ec0a5b04
Aaron Stroud <astroud@gmail.com>
2021-02-17 12:52:04 -0800

    feat: Connect to mongoDB
    Covers lesson up to Exercises 3.13-3.14
    - add dotenv and mongoose dependencies
    - connects to MongoDB
    - uses environment variables
    - includes mongo.js tool for accessing database from command line

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
+const app = express()
+const Note = require('./models/note')
 // const cors = require('cors')
 
-const app = express()
 
 
 // app.use(cors())
@@ -48,18 +50,15 @@ let notes = [
   })
   
   app.get('/api/notes/', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
 
   app.get('/api/notes/:id', (request, response) => {
-    let id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => {
-      console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-      return note.id === id
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
     })
-    console.log(note)
-    response.json(note)
   })
   
   app.delete('/api/notes/:id', (request, response) => {
@@ -79,28 +78,25 @@ let notes = [
   app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
       important: body.important || false,
       date: new Date(),
-      id: generateId(),
-    }
+    })
   
-    notes = notes.concat(note)
-  
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
 
   
   app.use(unknownEndpoint)
 
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)

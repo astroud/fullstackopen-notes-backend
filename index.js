52d858a8c77ed4debf918c5820f652511d78971f
Aaron Stroud <astroud@gmail.com>
2021-02-22 12:03:51 -0800

    feat: add error handling & note updating
    Rest of Part 3c

--- index.js
+++ index.js
@@ -55,17 +55,39 @@ let notes = [
     })
   })
 
-  app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
+  app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
   })
   
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
   
-    response.status(204).end()
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
   })
 
   const generateId = () => {
@@ -93,9 +115,21 @@ let notes = [
     })
   })
 
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
   
-  app.use(unknownEndpoint)
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
 
+
+  // handles requests with unknown endpoint
+  app.use(unknownEndpoint)
+  app.use(errorHandler)
+  
   const PORT = process.env.PORT
 
   app.listen(PORT, () => {
